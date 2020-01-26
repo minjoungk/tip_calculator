d5e7e4df040f9ac23263d709f39b2899a24b3d7a
minjoungk <minjoungk@gmail.com>
2020-01-26 15:44:03 -0800

    Initial Commit

---
+++ tip_calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tip_calculator
+//
+//  Created by MinJoung Kim on 1/26/20.
+//  Copyright © 2020 MinJoung Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
