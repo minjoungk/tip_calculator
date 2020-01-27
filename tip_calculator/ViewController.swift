93a877f7df9bc4918d2fe58bb532d91623812826
minjoungk <minjoungk@gmail.com>
2020-01-26 17:53:20 -0800

    complete basic features of tip Calculator app

--- tip_calculator/ViewController.swift
+++ tip_calculator/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var BillAmount: UITextField!
+    @IBOutlet weak var TipAmount: UILabel!
+    @IBOutlet weak var TotalAmount: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func on_Tap(_ sender: Any) {
+        print("Hello")
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let tipPercentage = [0.15, 0.18, 0.2]
+        let bill = Double(BillAmount.text!) ?? 0
+        let tip = bill * tipPercentage[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        TipAmount.text = String(format: "$%.2f", tip)
+        TotalAmount.text = String(format: "$%.2f", total)
+    }
+    
 
 }
-
